60ab90642c2d19b2675ed290fd88380a4df2ad23
Sashwat K <sashwat0001@gmail.com>
2019-10-28 19:15:54 +0530

    Added header files

---
+++ src/arduino-code/arduino-code.ino
@@ -0,0 +1,14 @@
+#include <SPI.h> // For SPI communication for OLED
+#include <Wire.h> // For i2C communication for OLED
+#include <Adafruit_GFX.h> // OLED library
+#include <Adafruit_SSD1306.h> // OLED library
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
